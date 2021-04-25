60106dbb82b8d357dc75eeeda039bb87e884eaa2
Ricardo Leal <rick.leal420@gmail.com>
2021-04-25 12:11:59 -0500

    New post and fixes

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ particlesJS.load('particles-js', 'particles.json', function() {
 */
 
 /* Otherwise just put the config content (json): */
-var toggle = document.getElementById("dark-mode-toggle");
-var darkTheme = document.getElementById("dark-mode-theme");
 
 particlesJS('particles-js',
   
